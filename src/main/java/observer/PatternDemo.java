9e185a5f926534ce80ecde2a954085671507dd52
Rrch <rrch.cr8@gmail.com>
2019-12-19 00:47:02 -0400

    basic functionality added

--- src/main/java/observer/PatternDemo.java
+++ src/main/java/observer/PatternDemo.java
@@ -5,11 +5,15 @@ public class PatternDemo {
     Sensor sensor = new Sensor();
 
     new DeviceOne(sensor);
-    new DeviceTwo(sensor);
+   Device d= new DeviceTwo(sensor);
 
     System.out.println("First state change: A");
     sensor.setState("A");
     System.out.println("Second state change: B");
     sensor.setState("B");
+
+    sensor.removeDevice(d);
+    System.out.println("Second state change: c");
+    sensor.setState("C");
   }
 }
